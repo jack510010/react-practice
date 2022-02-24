a4b7e0ba74512377422f796c4516453a6e033d77
Albert <jack25512909@gmail.com>
2022-02-24 22:23:47 +0800

    寫一支公版的.class css，補上其他沒共用的 css。

---
+++ src/components/Card.js
@@ -0,0 +1,20 @@
+import './Card.css'
+
+function Card(props) {
+  const classes = 'card ' + props.className
+  return <div className={classes}>{props.children}</div>
+  /*
+    the value of this special children props will always be the content between the opening and closing tags of your custom components.
+    這個 props.children 是內建的。
+
+    custom components only support what you tell them to support. So we have tweak the code in the card component.
+    意思是共用的 .card{css} 可以提取出來沒錯，做一個公版給大家用，但還要再補上其他沒共用的 css ，不然會跑版， 譬如補上 font-size or display: flex 的class等等之類。
+
+    
+    『 const classes = 'card ' + props.className 』這串意思是 公版的card class + 其他的 class。
+    然後！！！再下『 className={classes} 』
+    完成之後， <Card className="expense-item"></Card> 這串就會變被看成是 className="card expense-item"
+  */
+}
+
+export default Card
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,6 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
+import Card from './Card'
 
 function ExpenseItem(props) {
   //這個通常取名叫props， 這樣比較不容易搞混。
@@ -9,13 +10,17 @@ function ExpenseItem(props) {
   */
 
   return (
-    <div className="expense-item">
+    /*
+        the value of this special children props will always be the content between the opening and closing tags of your custom components.
+        <Card></Card> 會像 wrapper 一樣包起來，這樣 Card 包住的都能吃到 Card.css ，需要寫一隻 Card.js 來做這個 wrapper
+    */
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   )
 }
 
