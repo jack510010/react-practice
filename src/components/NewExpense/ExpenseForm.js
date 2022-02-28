a61dd663a543f10481ef95700e4a694f045614a1
Albert <jack25512909@gmail.com>
2022-02-28 16:46:28 +0800

    第一版 ExpenseForm 表單測試

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,52 +3,59 @@ import React, { useState } from 'react'
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
-  //   const [enteredTitle, setEnteredTitle] = useState('')  可以這樣寫，但是今天如果有 100個 變數，你就不會這樣寫
-  //   const [enteredAmount, setEnteredAmount] = useState('')  可以這樣寫，但是今天如果有 100個 變數，你就不會這樣寫
-  //   const [enteredDate, setEnteredDate] = useState('')  可以這樣寫，但是今天如果有 100個 變數，你就不會這樣寫
+  const [enteredTitle, setEnteredTitle] = useState('') // 可以這樣寫，但是今天如果有 100個 變數，你就不會這樣寫
+  const [enteredAmount, setEnteredAmount] = useState('') // 可以這樣寫，但是今天如果有 100個 變數，你就不會這樣寫
+  const [enteredDate, setEnteredDate] = useState('') // 可以這樣寫，但是今天如果有 100個 變數，你就不會這樣寫
 
-  const [userInput, setUserInput] = useState({
-    // 100個 變數的寫法，你會這樣寫
-    enteredTitle: '',
-    enteredAmount: '',
-    enteredDate: '',
-  })
+  //   const [userInput, setUserInput] = useState({
+  //     100個 變數的寫法，你會這樣寫
+  //     enteredTitle: '',
+  //     enteredAmount: '',
+  //     enteredDate: '',
+  //   })
 
   const titleChangeHandler = (e) => {
-    // console.log('Title changed~~') //測試效果，沒錯是我要的
-    // console.log(e.target.value)    測試效果，沒錯是我要的
-
-    // setEnteredTitle(e.target.value) 1、2個變數的寫法
-
-    setUserInput({
-      // 100個 變數的寫法
-
-      ...userInput,
-      //! 這裡前面一定要寫『 ...userInput 』， 如果光是單單寫 『enteredTitle: e.target.value』的話， enteredAmount、enteredDate 這兩個變數就會被丟掉。
-
-      enteredTitle: e.target.value,
-    })
+    setEnteredTitle(e.target.value) // 1、2個變數的寫法
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: e.target.value,
+    // })
+    // setUserInput((preState) => {
+    // 這串看一下 z筆記.txt， 筆記位置是 setUserInput((preState)
+    //   return { ...preState, enteredTitle: e.target.value }
+    // })
   }
 
   const amountChangeHandler = (e) => {
-    // setEnteredAmount(e.target.value)
-
-    setUserInput({
-      ...userInput,
-      enteredAmount: e.target.value,
-    })
+    setEnteredAmount(e.target.value)
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: e.target.value,
+    // })
   }
 
   const dateChangeHandler = (e) => {
-    // setEnteredDate(e.target.value)
-    setUserInput({
-      ...userInput,
-      enteredDate: e.target.value,
-    })
+    setEnteredDate(e.target.value)
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: e.target.value,
+    // })
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    }
+
+    console.log(expenseData) // 測試是否有效果
   }
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__controls">
           <label>Title</label>
