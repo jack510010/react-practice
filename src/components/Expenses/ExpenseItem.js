a716296f12297b1d8c73157d1d4d4ce2832e193d
Albert <jack25512909@gmail.com>
2022-03-01 11:10:00 +0800

    因為被包在<ul>裡面，card外面加上<li>會更好

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -12,13 +12,15 @@ function ExpenseItem(props) {
   return (
     //the value of this special children props will always be the content between the opening and closing tags of your custom components.
     //<Card></Card> 會像 wrapper 一樣包起來，這樣 Card 包住的都能吃到 Card.css ，需要寫一隻 Card.js 來做這個 wrapper
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+      </Card>
+    </li>
   )
 }
 
